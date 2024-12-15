570f511d5bacd887e258a2073d60282b383adbbd
Dit05 <mullerkrisztianbusiness@gmail.com>
2024-12-15 15:24:23 +0100

    Express/Swagger főzőlap

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
+import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
-console.log("hello word")
+
+const app = express();
+
+
+const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Időjárás 2',
+      version: '1.0.0'
+    },
+  },
+  apis: ['./routes/*.ts'], // Specify the paths to your route files
+});
+
+// Serve the Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
